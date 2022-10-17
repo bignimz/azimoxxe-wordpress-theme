4a96f5655fb0aed01475bd8386e1118b56636e11
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-10-17 12:17:40 +0300

    added wp_head and wp_footer hook functions to hook in the styles and scripts for the theme

--- header.php
+++ header.php
@@ -8,12 +8,13 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(); ?>">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset') ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Azimoxxe WordPress Theme</title>
+
+    <?php wp_head(); ?>
 </head>
 <body>
     <header>Welcome to Azimoxxe WordPress Theme</header>
